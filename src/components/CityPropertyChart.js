dec4278c1c3f4ea400140bbb7ae30bd7c39571e5
Yohann Durand <yohanndurand76@gmail.com>
2023-11-01 12:16:03 +0100

    add chart for CityPropertyChart component

--- src/components/CityPropertyChart.js
+++ src/components/CityPropertyChart.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Pie } from 'react-chartjs-2';
+import Chart from 'chart.js/auto';
 
 /*
 This component calculates the % of the city in the portfolio
@@ -6,10 +8,8 @@ For the moment, cities must be in the constant for this to work. In the future, 
 this component calculates according to the total value of the city in the portfolio.
 */
 
-// TODO : make a graph
-
 function PropertyPercentage({ properties }) {
-  const cities = ['Detroit', 'Montgomery', 'Chicago', 'Cleveland', 'Los Santos', 'Covington', 'Toledo', 'Rochester', 'Highland Park'  ];
+  const cities = ['Detroit', 'Montgomery', 'Chicago', 'Cleveland', 'Los Santos', 'Covington', 'Toledo', 'Rochester', 'Highland Park'];
 
   const cityValues = cities.reduce((values, city) => {
     values[city] = 0;
@@ -31,16 +31,39 @@ function PropertyPercentage({ properties }) {
     return total + propertyValue;
   }, 0);
 
+  const labels = cities;
+  const data = labels.map((city) =>
+    ((cityValues[city] / totalPortfolioValue) * 100).toFixed(2)
+  );
+
+  const chartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        data: data,
+        backgroundColor: ['#FF5733', '#FFC300', '#C70039', '#900C3F', '#581845', '#FF5733', '#FFC300', '#C70039', '#900C3F'],
+      },
+    ],
+  };
+
   return (
     <div className="property-percentage">
       <h2>Pourcentage de la valeur du portefeuille par ville :</h2>
       <ul>
-        {cities.map((city) => (
+        {labels.map((city) => (
           <li key={city}>
             {city}: {((cityValues[city] / totalPortfolioValue) * 100).toFixed(2)}%
           </li>
         ))}
       </ul>
+      <div style={{ maxWidth: '400px' }}>
+        <Pie data={chartData} options={chartOptions} />
+      </div>
     </div>
   );
 }
