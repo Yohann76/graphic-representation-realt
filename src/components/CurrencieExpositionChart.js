68ba72f91fc4283689a68e3aba8dd4446a7f5cc7
Yohann Durand <yohanndurand76@gmail.com>
2023-11-23 15:58:07 +0100

    add new color for graphic

--- src/components/CurrencieExpositionChart.js
+++ src/components/CurrencieExpositionChart.js
@@ -46,7 +46,23 @@ function CurrencieExposition({ properties }) {
     datasets: [
       {
         data: data,
-        backgroundColor: ['#FF5733', '#FFC300', '#C70039', '#900C3F', '#581845'],
+        backgroundColor: [
+          '#C1C2C5',
+          '#A6A7AA',
+          '#8B8C8F',
+          '#707173',
+          '#555659',
+          '#3A3B3E',
+          '#1F2023',
+          '#D3D4D7',
+          '#B8B9BC',
+          '#9DA0A3',
+          '#838588',
+          '#686B6E',
+          '#4E5053',
+          '#333537',
+          '#18191C',
+        ],
       },
     ],
   };
--- src/components/TypePropertyChart.js
+++ src/components/TypePropertyChart.js
@@ -50,7 +50,23 @@ function TypePropertyChart({ properties }) {
     datasets: [
       {
         data: data,
-        backgroundColor: ['#FF5733', '#FFC300', '#C70039'],
+        backgroundColor: [
+          '#C1C2C5',
+          '#A6A7AA',
+          '#8B8C8F',
+          '#707173',
+          '#555659',
+          '#3A3B3E',
+          '#1F2023',
+          '#D3D4D7',
+          '#B8B9BC',
+          '#9DA0A3',
+          '#838588',
+          '#686B6E',
+          '#4E5053',
+          '#333537',
+          '#18191C',
+        ],
       },
     ],
   };
