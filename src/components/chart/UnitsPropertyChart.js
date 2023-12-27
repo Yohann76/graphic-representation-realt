4290b42a4b231cd0cd1cc25d57cc34053713d60e
Yohann Durand <yohanndurand76@gmail.com>
2023-12-27 16:36:33 +0100

    fix bug: update chart portfolio value by location

--- src/components/chart/UnitsPropertyChart.js
+++ src/components/chart/UnitsPropertyChart.js
@@ -15,18 +15,16 @@ function UnitsPropertyChart({ properties }) {
   });
 
   const percentageRentedUnits = totalUnits > 0 ? ((totalRentedUnits / totalUnits) * 100).toFixed(2) : 0;
-
-  const rentedUnitsPercentage = parseFloat(percentageRentedUnits);
-  const notRentedUnitsPercentage = parseFloat(100 - percentageRentedUnits);
+  const percentageNotRentedUnits = (100 - percentageRentedUnits).toFixed(2);
 
   const chartData = {
     labels: [
-      `Rented units: ${isNaN(rentedUnitsPercentage) ? 0 : rentedUnitsPercentage.toFixed(2)}%`,
-      `Not rented units: ${isNaN(notRentedUnitsPercentage) ? 0 : notRentedUnitsPercentage.toFixed(2)}%`,
+      `Rented units: ${percentageRentedUnits}%`,
+      `Not rented units: ${percentageNotRentedUnits}%`,
     ],
     datasets: [
       {
-        data: [percentageRentedUnits, 100 - percentageRentedUnits],
+        data: [percentageRentedUnits, percentageNotRentedUnits],
         backgroundColor: [
           '#00BFFF',
           '#1E90FF',
@@ -48,72 +46,88 @@ function UnitsPropertyChart({ properties }) {
     ],
   };
 
+
   const chartOptions = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: false,
-        position: 'right',
-        labels: {
-          generateLabels: function (chart) {
-            const data = chart.data;
-            if (data.labels.length && data.datasets.length) {
-              const legendLabels = data.labels.map((label, index) => {
-                const backgroundColor = data.datasets[0].backgroundColor[index];
-                return {
-                  text: label,
-                  fillStyle: backgroundColor,
-                };
-              });
-              return legendLabels;
-            }
-            return [];
-          },
-          color: 'white',
-        },
-      },
-      title: {
-        display: false,
-        text: 'Breakdown of portfolio value by location',
-        fontSize: 16,
-      },
-    },
-  };
+     responsive: true,
+     maintainAspectRatio: false,
+     plugins: {
+       legend: {
+         display: false,
+         position: 'right',
+         labels: {
+           generateLabels: function (chart) {
+             const data = chart.data;
+             if (data.labels.length && data.datasets.length) {
+               const legendLabels = data.labels.map((label, index) => {
+                 const backgroundColor = data.datasets[0].backgroundColor[index];
+                 const percentage = data.datasets[0].data[index];
 
-  const legendItems = chartData.labels.map((label, index) => {
-    const backgroundColor = chartData.datasets[0].backgroundColor[index];
-    const percentage = [percentageRentedUnits, 100 - percentageRentedUnits][index];
+                 const dotStyle = {
+                   backgroundColor: backgroundColor,
+                 };
 
-    const dotStyle = {
-      backgroundColor: backgroundColor,
-    };
+                 const textStyle = {
+                   color: backgroundColor,
+                 };
 
-    const textStyle = {
-      color: backgroundColor,
-    };
+                 const formattedPercentage = label === 'Rented units' ? `${percentage}%` : `${percentage}%`;
 
-    return (
-      <div key={label}>
-        <span className="legend-color-dot" style={dotStyle}></span>
-        <span className="legend-label" style={textStyle}>
-          {`${label}: ${percentage}%`}
-        </span>
-      </div>
-    );
-  });
+                 return {
+                   text: `${label}: ${formattedPercentage}`,
+                   fillStyle: backgroundColor,
+                 };
+               });
+               return legendLabels;
+             }
+             return [];
+           },
+           color: 'white',
+         },
+       },
+       title: {
+         display: false,
+         text: 'Breakdown of portfolio value by location',
+         fontSize: 16,
+       },
+     },
+   };
 
-  return (
-    <div className="component-graph section">
-      <h2>Breakdown of portfolio value by location</h2>
-      <div className="graph-and-legend">
-        <div class="graph">
-          <Pie data={chartData} options={chartOptions} />
-        </div>
-        <div className="legend">{legendItems}</div>
-      </div>
-    </div>
-  );
+   const legendItems = chartData.labels.map((label, index) => {
+     const backgroundColor = chartData.datasets[0].backgroundColor[index];
+     const percentage = chartData.datasets[0].data[index];
+
+     const dotStyle = {
+       backgroundColor: backgroundColor,
+     };
+
+     const textStyle = {
+       color: backgroundColor,
+     };
+
+     return (
+       <div key={label}>
+         <span className="legend-color-dot" style={dotStyle}></span>
+         <span className="legend-label" style={textStyle}>
+           {`${label}: ${percentage}%`}
+         </span>
+       </div>
+     );
+   });
+
+   return (
+     <div className="component-graph section">
+       <h2>Breakdown of portfolio value by location</h2>
+       <div className="graph-and-legend">
+         <div className="graph">
+           <Pie data={chartData} options={chartOptions} />
+         </div>
+         <div className="legend">
+           {legendItems}
+         </div>
+       </div>
+     </div>
+   );
+
 }
 
 export default UnitsPropertyChart;
