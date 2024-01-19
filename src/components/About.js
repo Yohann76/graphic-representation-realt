1788622fbc091eb7de052fb6952520a450de0268
Yohann Durand <yohanndurand76@gmail.com>
2024-01-19 15:15:02 +0100

    deplace dashbord logic in its component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>À propos de nous</h1>
+      <p>Ceci est la page à propos de notre application.</p>
+    </div>
+  );
+};
+
+export default About;
